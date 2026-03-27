1b1bf7694e1b3e6dc7d952408e03c7524fe590c8
Ramalho Maria (SO/ETS-BR) <ram3jvl@bosch.com>
2026-03-27 11:45:16 -0300

    Atualizando calculadora

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ int valor1 = int.Parse(Console.ReadLine());
         Console.WriteLine("2- Subtrair");
         Console.WriteLine("3- Multiplicar");
         Console.WriteLine("4- Dividir");
+        Console.WriteLine("5- Resto da divisão");
         Console.WriteLine("0- Sair");
 
 Console.WriteLine("Escolha a opção:");
@@ -47,6 +48,14 @@ switch(opcao)
        }
     break;
 
+        case 5:
+        if( valor1 == 0 || valor2 == 0)
+        {
+         Console.WriteLine("Não é possivel dividir por zero");
+        } else {
+            Console.WriteLine("Resultado:" + (valor1 % valor2));
+        }
+        break;
   default:
     Console.WriteLine("Opção inválida");
     break;
